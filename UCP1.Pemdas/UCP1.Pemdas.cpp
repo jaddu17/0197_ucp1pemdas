b4a9684452ba053e9f7cc96ed828108745cf6535
jaddu17 <shafajaddu@gmail.com>
2024-05-05 17:07:07 +0700

    Add project files.

---
+++ UCP1.Pemdas/UCP1.Pemdas.cpp
@@ -0,0 +1,26 @@
+// 1. int x; , string desa; , nBilangan
+// 2. void nama prosedur -> void inputData()
+//    tipe data nama fungsi()
+//	  {
+//		return yang dikembalikkan
+//	  }
+// 3. kondisi yang di ulang-ulang
+//	while (kondisi)
+//  {
+//	  proses yang di ulang
+//  }
+// 4. srand(time(0))
+// 5. struct (nama data)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int nKotaA;
+	int nKotaB;
+	string status;
+
+	if (nKotaA > 75) {
+		status = "Tidak Sehat"
+	}
+}
